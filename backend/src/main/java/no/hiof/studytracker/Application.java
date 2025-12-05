11e63c5320b184e1c296b7c7dab2917146d82de5
Abdullahi Ahmed <120040059+Abdis10@users.noreply.github.com>
2025-12-05 22:29:04 +0100

    Add DB connector with automatic migrations
    - Added migrate() to create tables on startup
    - Added getConnection() for reusable DB access
    - Improved error handling and logging

--- backend/src/main/java/no/hiof/studytracker/Application.java
+++ backend/src/main/java/no/hiof/studytracker/Application.java
@@ -1,12 +1,16 @@
 package main.java.no.hiof.studytracker;
 
-import no.hiof.studytracker.database.DB;
+import main.java.no.hiof.studytracker.database.DB;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Application {
-    public static void main(String[] args) {
-        try (var conn = DB.connect()) {
+
+    public static void main(String[] args) throws SQLException {
+        // Kjør migrations ved oppstart
+        DB.migrate();
+        try (Connection conn = DB.getConnection()) {
             System.out.println("SQLite fungerer!");
         } catch (SQLException e) {
             e.printStackTrace();
--- backend/src/main/java/no/hiof/studytracker/database/DB.java
+++ backend/src/main/java/no/hiof/studytracker/database/DB.java
@@ -1,21 +1,39 @@
-package no.hiof.studytracker.database;
+package main.java.no.hiof.studytracker.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DB {
     private static final String URL = "jdbc:sqlite:database/studytracker.db";
 
-    public static Connection connect() throws SQLException {
-        try {
-            return DriverManager.getConnection(URL);
+    // Kjør migrations (database‐endringer som er lagret som kode), Kalles kun ved app-oppstart
+    public static void migrate() throws SQLException {
+        try (Connection conn = DriverManager.getConnection(URL)) {
+            Statement stmt = conn.createStatement();
+
+            String sql = """
+                CREATE TABLE IF NOT EXISTS sessions (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    date TEXT NOT NULL,
+                    hours INTEGER NOT NULL,
+                    productivity INTEGER,
+                    comment TEXT
+                );
+            """;
+
+            stmt.execute(sql);     // migration
+            System.out.println("Database Migrations fullført.");
         }
         catch (Exception e) {
-            throw new RuntimeException("Kunne ikke koble til SQlite", e);
+            System.err.println("Database Migration-feil: " + e.getMessage());
+            throw new RuntimeException("Kunne ikke koble til SQlite");
         }
 
+    }
 
-
+    public static Connection getConnection()throws SQLException {
+        return DriverManager.getConnection(URL);
     }
 }
