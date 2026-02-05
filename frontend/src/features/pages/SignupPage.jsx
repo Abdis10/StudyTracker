585af623019b538c414e4b62d1edb8979d35b00e
Abdullahi Ahmed <120040059+Abdis10@users.noreply.github.com>
2026-02-05 23:49:56 +0100

    feat: created navbar - navbar is currently in signupPage just for testing

--- frontend/src/features/pages/SignupPage.jsx
+++ frontend/src/features/pages/SignupPage.jsx
@@ -1,10 +1,15 @@
 import SignupForm from "../auth/components/SignupForm.jsx";
 import "../auth/css/LoginSignup.css"
+import Navbar from "../components/Navbar.jsx";
 
 
 function SignupPage() {
     return (
-        <SignupForm />
+        <>
+            <Navbar/>
+            <SignupForm />
+        </>
+
     )
 }
 
