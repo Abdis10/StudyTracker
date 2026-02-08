6c1b4d2d5d2bc5889027f59ed5378398b3005551
Abdullahi Ahmed <120040059+Abdis10@users.noreply.github.com>
2026-02-08 01:47:05 +0100

    feat(signup): improve signup form layout and validation
    - add labels for all inputs
    - add live validation for password confirmation
    - display backend error messages in UI

--- frontend/src/features/pages/LoginPage.jsx
+++ frontend/src/features/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import LoginForm from "../auth/components/LoginForm.jsx";
-import "../auth/css/LoginSignup.css"
+import "../auth/css/loginForm.css"
 
 function LoginPage() {
     return (
