9de3f309fd2dec3d92ea17f55f0543aa92977183
Abdullahi Ahmed <120040059+Abdis10@users.noreply.github.com>
2026-04-21 23:56:46 +0200

    feat: Add subject summary data fetching

--- frontend/src/features/components/SubjectSummaryBar.jsx
+++ frontend/src/features/components/SubjectSummaryBar.jsx
@@ -1,17 +1,31 @@
 import "../css/subjectSummaryBar.css";
+import {useEffect, useMemo, useState} from "react";
 
-function SubjectSummaryBar() {
+function SubjectSummaryBar({ sessionData = [], subjectData = []} ) {
+    const subjectsLength = subjectData?.length || 0;
+
+    const summary = useMemo(() => {
+        const subjectIdsUsed = new Set(
+            sessionData.filter((s) => s.subjectId !== null && s.subjectId !== undefined)
+                .map((s) => s.subjectId)
+        );
+
+        return {
+            subjectIdsUsed
+        }
+    }, [subjectData, sessionData]);
+
     return (
         <div className="subjects-summary">
             <h2>Subjects summary</h2>
             <div className="summary-bar">
                 <div className="summary-card">
-                    <p className="summary-label">Total Subjects</p>
+                    <p className="summary-label">Total Subjects {subjectsLength}</p>
                     <h3>{}</h3>
                 </div>
 
                 <div className="summary-card">
-                    <p className="summary-label">Subjects Used</p>
+                    <p className="summary-label">Subjects Used {summary.subjectIdsUsed}</p>
                     <h3>{}</h3>
                 </div>
 
--- frontend/src/features/components/Subjects.jsx
+++ frontend/src/features/components/Subjects.jsx
@@ -1,10 +1,59 @@
 import ComingSoon from "./ComingSoon.jsx";
 import SubjectSummaryBar from "./SubjectSummaryBar.jsx";
+import useAuth from "../auth/useAuth.js";
+import {useEffect, useState} from "react";
+import {getSessions, getSubjects} from "../../api/sessionApi.js";
+import {logger} from "../utils/Logger.js";
+import {toast} from "react-hot-toast";
 
 function Subjects() {
+    const { isAuth } = useAuth();
+    const [sessions, setSessions] = useState();
+    const [subjects, setSubjects] = useState();
+
+    useEffect(() => {
+        if (isAuth) {
+            const sessionData = async () => {
+                try {
+                    const token = localStorage.getItem("token");
+                    const result = await getSessions(token);
+
+                    if (result.success) {
+                        setSessions(result.data);
+                    }
+                } catch (e) {
+                    logger.error("Fetching sessions failed:", e);
+                }
+            };
+            sessionData();
+        }
+    }, [isAuth]);
+
+    useEffect(() => {
+        if (isAuth) {
+            const subjectData = async () => {
+                try {
+                    const token = localStorage.getItem("token");
+                    const subjectResult = await getSubjects(token);
+
+                    if (subjectResult.success) {
+                        setSubjects(subjectResult.data);
+                    } else {
+                        toast.error(subjectResult.data.message);
+                    }
+                } catch (e) {
+                    logger.error("Register network error:", e);
+                    toast.error("Something went wrong with the connection to the server!");
+                }
+            };
+            subjectData();
+        }
+    }, [isAuth]);
+
+
     return (
         <>
-            <SubjectSummaryBar />
+            <SubjectSummaryBar sessionData={sessions} subjectData={subjects} />
         </>
     );
 }
